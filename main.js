20898724c610c2290e68f4df9781f2f47f6057fc
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2025-05-21 19:59:49 -0400

    Añadiendo toggle bar a version mobile

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import './sass/style.scss';
+// Este código de abajo me ayuda a abrir el menú de hamburguesa
+const toggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
 
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
 
